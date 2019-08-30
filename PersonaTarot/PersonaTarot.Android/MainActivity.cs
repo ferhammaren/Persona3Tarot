20abdb84aed1feaa4a75f72fbb2577287ded753d
ferhammaren <fernanda.meza.lopez@gmail.com>
2019-08-30 16:01:44 -0700

    Yea bby

---
+++ PersonaTarot/PersonaTarot.Android/MainActivity.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace PersonaTarot.Droid
+{
+    [Activity(Label = "PersonaTarot", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {  //aklsuidfhowuhef
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+    }
+}
---
+++ PersonaTarot/PersonaTarot/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace PersonaTarot
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            
+            InitializeComponent();
+            var player = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+            player.Load("bgm.mp3");
+            player.Play();
+
+        }
+
+
+        async void OnSpreadClicked(object sender,EventArgs args)
+        {
+            var clickSound = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            clickSound.Load("Files/sfx/optionSelect.wav");
+            clickSound.Play();
+            await Navigation.PushAsync(new SelectSpread());
+        }
+
+        async void OnSettingsClicked(object sender, EventArgs args)
+        {
+            var clickSound = CrossSimpleAudioPlayer.CreateSimpleAudioPlayer();
+            clickSound.Load("Files/sfx/optionSelect.wav");
+            clickSound.Play();
+            await Navigation.PushAsync(new TweakSettings());
+        }
+
+    }
+}
